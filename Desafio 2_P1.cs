459fe1fd49b9b38419e793cfc6fa10c855ae8983
Damark13 <53448123+Damark13@users.noreply.github.com>
2020-07-10 15:20:50 -0500

    Add files via upload

---
+++ Desafio 2_P1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DATOS
+            double y = 2;
+            double z = 3;
+            double pi = 3.1416;
+
+
+            // PROCESOS
+            double t = Math.Sqrt(Math.Pow(y, 2) + Math.Pow(z, 2));
+            double a0 = Math.Atan(y / z);
+            double c0 = Math.Atan(z / y);
+
+            double a1 = (a0 * 180) / pi;
+            double c1 = (c0 * 180) / pi;
+
+
+            // SALIDA
+            Console.WriteLine("Hipotenusa(t) = " + t);
+            Console.WriteLine("Angulo a = " + a1);
+            Console.WriteLine("Angulo c = " + c1);
+        }
+    }
+}
