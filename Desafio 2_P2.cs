48a48b8876cc96cf55cfe17464bcfa6a76a11161
Damark13 <53448123+Damark13@users.noreply.github.com>
2020-07-12 18:57:37 -0500

    Add files via upload

---
+++ Desafio 2_P2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DATOS
+            double t = 3.61;
+            double a = 33.69;
+            double pi = 3.1416;
+
+
+            // PROCESOS
+            double rada = a * (pi / 180);
+            double y = Math.Sin(rada) * t;
+            double z = Math.Sqrt((t * t) - (y * y));
+            double c0 = Math.Atan(z / y);
+            double c1 = (c0 * 180) / pi;
+
+
+            // SALIDA
+            Console.WriteLine("Lado Y = " + y);
+            Console.WriteLine("Lado z = " + z);
+            Console.WriteLine("Angulo a = " + a);
+            Console.WriteLine("Angulo c = " + c1);
+        }
+    }
+}
