fd8dd48412198b4f5dd830dc1792267d1e5fcd57
Damark13 <53448123+Damark13@users.noreply.github.com>
2020-07-13 14:33:45 -0500

    Add files via upload

---
+++ Desafio 01 _P1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // DATOS
+            Console.WriteLine("Recuerde escribir decimales con coma (,)");
+            
+
+            Console.Write("Valor de b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Valor de y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Valor de z: ");
+            double z = double.Parse(Console.ReadLine());
+
+
+            // PROCESOS
+            double radb = b * 180 / Math.PI;
+            double a = Math.Atan(y / z);
+            double t = Math.Sqrt((y * y) + (z * z));
+            double abgrados = (a + radb) * Math.PI /180;
+            double d = 180 - 90 - abgrados;
+
+            double x = (t * Math.Sin(b)) / Math.Sin(d);
+
+            // SALIDA
+            Console.WriteLine("Valor de x: " + x);
+
+
+        }
+    }
+}
