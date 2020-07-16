80543bfedcfc0678a5b1c922f4cab0e4ce716363
Damark13 <53448123+Damark13@users.noreply.github.com>
2020-07-16 17:38:43 -0500

    Add files via upload

---
+++ Cálculo de IMC (16-07-2020).cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ENTRADAS
+
+            Console.Write("Ingrese su estatura (en metros): ");
+            double alt = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese su peso actual (en kilogramos): ");
+            double pes = double.Parse(Console.ReadLine());
+
+            // PROCESOS
+            double imc = pes / (alt * alt);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Subir de peso");
+            }
+            else if (18.5 <= imc && imc < 25)
+            {
+                Console.WriteLine("Saludable");
+            }
+            else if (25 <= imc && imc < 30)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
+            else if (imc >= 30)
+            {
+                Console.WriteLine("Obesidad");
+            }
+        }
+    }
+}
