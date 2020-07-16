097e14de64ad41fb35fcd6d779f67171f5a70bb9
Damark13 <53448123+Damark13@users.noreply.github.com>
2020-07-16 16:23:26 -0500

    Add files via upload

---
+++ Cálculo de Tarifa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cuántos smmlv gana por su empleo?: ");
+            double salario = double.Parse(Console.ReadLine());
+
+
+            if (salario < 2)
+            {
+                Console.WriteLine("Tarifa A");
+            }
+
+            else if (2 <= salario && salario < 4)
+            {
+                Console.WriteLine("Tarifa B");
+            }
+
+            else
+            {
+                Console.WriteLine("Tarifa C");
+            }
+        }
+    }
+}
